f55dc43d8e4ac79c43e46bcb80fd53c151f35395
Markus Ang <maokooss@gmail.com>
2021-06-25 22:48:15 +0800

    added selfBan catch

--- frontend/src/app/containers/UserDetailPage/saga.ts
+++ frontend/src/app/containers/UserDetailPage/saga.ts
@@ -12,6 +12,7 @@ export function* loadUser(action: PayloadAction<String>) {
     yield put(actions.saveUser(user.data));
   } catch (err) {
     toast.error('Failed to load user');
+    console.log(err);
   }
 }
 
